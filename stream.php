3eedadadb41feaada061a254c459321cce35c706
Ella Yu <ella@cloudflare.com>
2020-11-04 22:42:06 +0000

    On branch master  Changes to be committed: 	new file:   images/blog-geekyflare.jpg 	new file:   images/stream-geekyflare.jpg 	new file:   stream.php

---
+++ stream.php
@@ -0,0 +1,46 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>GeekyFlare | An Image Test Page</title>
+		<?php include('partials/head.php') ?>
+	</head>
+
+<body class="is-preload">
+
+<!-- Wrapper -->
+<div id="wrapper">
+
+<!-- Sticky Menu -->
+	<?php include('partials/sticky-menu.php') ?>
+
+<!-- Main -->
+<div id="main" class="alt">
+	<section id="one">
+		<div class="inner">
+			<div class="row">
+				<div class="col-6 col-12-small">
+					<header class="major"><h1>Stream</h1></header>
+				</div>	
+			</div>
+		<span class="image fit">
+			<h1 style="text-align: center; margin-bottom: 50px;">Hello, Cloudflare Stream<br> Alausí, Ecuador </h1>
+			<div style="position: relative; padding-top: 56.25%;"><iframe
+			src="https://iframe.videodelivery.net/3877faed13d5c02255dfdf06d1d89780"
+			style="border: none; position: absolute; top: 0; height: 100%; width: 100%;"
+			allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;"
+			allowfullscreen="true"></iframe>
+			</div>
+		</span>
+		</div>
+	</section>
+
+
+</div>
+
+<!-- Footer -->
+	<?php include('partials/footer.php') ?>
+
+</div>
+
+</body>
+</html>
